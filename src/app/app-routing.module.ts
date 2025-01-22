cc52d7f511fb6bb307960559eacdf54966f0eaf9
frank.wesemann <frank.wesemann@kreuzwerker.de>
2025-01-22 16:58:34 +0100

    feat:  before installing the lib

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {
+  CollectionCountHistoryComponent,
+  CountsWithHistoryComponent,
+  QualityMatrixComponent
+} from 'ngx-edu-sharing-metaqs2';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'quality-matrix', component: QualityMatrixComponent},
+  {
+    path: 'license-count-sources',
+    component: CountsWithHistoryComponent,
+    data: { title: 'pageTitle.license-count-sources', apiMethod: 'getLicenseCountsByReplicationSource' },
+  },
+  {
+    path: 'collection-count-history',
+    component: CollectionCountHistoryComponent,
+  },
+  { path: '',   redirectTo: '/quality-matrix', pathMatch: 'full' },
 
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpClient} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {NgMetaWidgetsLibModule} from 'ngx-edu-sharing-metaqs2';
+const environment = {
+  apiPath: 'https://metaqs-2.staging.openeduhub.net',
+  eduSharingPath: 'https://repository.staging.openeduhub.net/edu-sharing',
+  useDummyValues: false,
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    NgMetaWidgetsLibModule.forRoot({
+      eduSharingPath: environment.eduSharingPath,
+      apiPath: environment.apiPath,
+      useDummyValues: environment.useDummyValues,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
